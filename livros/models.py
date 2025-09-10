9d2ee336bb6e4e5b8d256dcc1d9fde19ac5c53c4
raphael31335 <raphel31ribeiro@gmail.com>
2025-09-10 13:53:07 -0300

    Atualizar o models.py

--- livros/models.py
+++ livros/models.py
@@ -1,13 +1,21 @@
+# livros/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Livro(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    google_id = models.CharField(max_length=100, null=True, blank=True)
+    google_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
     titulo = models.CharField(max_length=200)
     autores = models.CharField(max_length=200, blank=True)
     capa = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.titulo
+
+class LivroUsuario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=20,
         choices=[('Lido', 'Lido'), ('Lendo', 'Lendo'), ('Desejo ler', 'Desejo ler')],
@@ -22,9 +30,8 @@ class Livro(models.Model):
     data_leitura = models.DateField(null=True, blank=True)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['user', 'google_id'], name='unique_user_googleid')
-        ]
+        unique_together = ('user', 'livro')
 
     def __str__(self):
-        return self.titulo
+        return f'{self.livro.titulo} de {self.user.username}'
+
